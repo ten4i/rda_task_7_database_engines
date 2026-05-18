4a46c55c279a36c3f07b4715d05372a31f4352b5
ten4i <oleksijchunihin@gmail.com>
2026-05-18 23:39:27 +0300

    fixed engine ProductDesription, according to tests

--- task.sql
+++ task.sql
@@ -24,7 +24,7 @@ CREATE TABLE ProductDescription(
     Description VARCHAR (100),
     FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
-) ENGINE=InnoDB;
+) ENGINE=MyISAM;
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
 CREATE TABLE Logs(
